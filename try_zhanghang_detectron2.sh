3f2845bb855150b5bcc009e300d385b115158a0b
Linkun Chen <lkchen@google.com>
2020-05-17 21:28:58 -0700

    Add some comments

--- try_zhanghang_detectron2.sh
+++ try_zhanghang_detectron2.sh
@@ -8,12 +8,14 @@ cp lsi-faster-rcnn/data/kitti/lists/train_lsi.txt detectron2-ResNeSt/datasets/VO
 cp lsi-faster-rcnn/data/kitti/lists/val_lsi.txt detectron2-ResNeSt/datasets/VOC2012/ImageSets/Main/test.txt
 
 # Only use a few test data
-head -n 2 lsi-faster-rcnn/data/kitti/lists/val_lsi.txt > detectron2-ResNeSt/datasets/VOC2012/ImageSets/Main/test.txt
+head -n 10 lsi-faster-rcnn/data/kitti/lists/val_lsi.txt > detectron2-ResNeSt/datasets/VOC2012/ImageSets/Main/test.txt
 
+# Use trained model in case you just want to --eval-only
 cat detectron_model/detectron_model_* > detectron2-ResNeSt/output/model_final.pth
 
 (cd detectron2-ResNeSt/ && python3 tools/train_net.py --eval-only  \
   --config-file configs/PascalVOC-Detection/faster_rcnn_R_50_C4.yaml)
 
+# Store trained model, github doesn't like big binary file
 mkdir detectron_model -p
 (cd detectron_model && split -b 50M ../detectron2-ResNeSt/output/model_final.pth detectron_model_)
