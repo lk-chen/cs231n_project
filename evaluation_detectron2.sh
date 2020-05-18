57eaad3b6214e99beaac945b0c4ce20d09b50c0a
Linkun Chen <lkchen@google.com>
2020-05-17 21:16:27 -0700

    Copy model to this repo, print timstamp of result to evaluate

--- evaluation_detectron2.sh
+++ evaluation_detectron2.sh
@@ -7,4 +7,8 @@ ln -s `pwd`/training/label_2 eval_kitti/build/data/object/label_2
 mkdir eval_kitti/build/lists/ -p
 cp detectron2-ResNeSt/datasets/VOC2012/ImageSets/Main/test.txt eval_kitti/build/lists/
 
+echo eval results from
+echo `stat eval_kitti/build/results/exp1/data | grep 'Modify'`
+echo
+
 (cd eval_kitti/build && ./evaluate_object exp1 test)
--- try_zhanghang_detectron2.sh
+++ try_zhanghang_detectron2.sh
@@ -8,7 +8,12 @@ cp lsi-faster-rcnn/data/kitti/lists/train_lsi.txt detectron2-ResNeSt/datasets/VO
 cp lsi-faster-rcnn/data/kitti/lists/val_lsi.txt detectron2-ResNeSt/datasets/VOC2012/ImageSets/Main/test.txt
 
 # Only use a few test data
-head -n 10 lsi-faster-rcnn/data/kitti/lists/val_lsi.txt > detectron2-ResNeSt/datasets/VOC2012/ImageSets/Main/test.txt
+head -n 2 lsi-faster-rcnn/data/kitti/lists/val_lsi.txt > detectron2-ResNeSt/datasets/VOC2012/ImageSets/Main/test.txt
+
+cat detectron_model/detectron_model_* > detectron2-ResNeSt/output/model_final.pth
 
 (cd detectron2-ResNeSt/ && python3 tools/train_net.py --eval-only  \
   --config-file configs/PascalVOC-Detection/faster_rcnn_R_50_C4.yaml)
+
+mkdir detectron_model -p
+(cd detectron_model && split -b 50M ../detectron2-ResNeSt/output/model_final.pth detectron_model_)
