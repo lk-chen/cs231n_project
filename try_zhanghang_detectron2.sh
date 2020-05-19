a74b7d87a0d8438e89c4e01ebb77073919207529
Linkun Chen <lkchen_github@outlook.com>
2020-05-19 07:39:37 +0000

    Check-in result of resnest-50
    Note: trained with 1000 data, so overfitting

--- try_zhanghang_detectron2.sh
+++ try_zhanghang_detectron2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Ensure you install with the steps in ipynb
-# RCNN has c4/fpn, I don't know the diff yet
 # https://github.com/facebookresearch/detectron2/tree/master/configs/PascalVOC-Detection
 rm detectron2-ResNeSt/datasets/VOC2012
 ln -s `pwd`/kitti-voc/VOC2012 detectron2-ResNeSt/datasets/VOC2012
@@ -8,14 +7,18 @@ cp lsi-faster-rcnn/data/kitti/lists/train_lsi.txt detectron2-ResNeSt/datasets/VO
 cp lsi-faster-rcnn/data/kitti/lists/val_lsi.txt detectron2-ResNeSt/datasets/VOC2012/ImageSets/Main/test.txt
 
 # Only use a few test data
-head -n 10 lsi-faster-rcnn/data/kitti/lists/val_lsi.txt > detectron2-ResNeSt/datasets/VOC2012/ImageSets/Main/test.txt
+# head -n 1000 lsi-faster-rcnn/data/kitti/lists/train_lsi.txt > detectron2-ResNeSt/datasets/VOC2012/ImageSets/Main/trainval.txt
+# head -n 10 lsi-faster-rcnn/data/kitti/lists/val_lsi.txt > detectron2-ResNeSt/datasets/VOC2012/ImageSets/Main/test.txt
 
 # Use trained model in case you just want to --eval-only
 cat detectron_model/detectron_model_* > detectron2-ResNeSt/output/model_final.pth
 
-(cd detectron2-ResNeSt/ && python3 tools/train_net.py --eval-only  \
-  --config-file configs/kitti/kitti.yaml)
+(cd detectron2-ResNeSt/ && python3 tools/train_net.py \
+  --config-file configs/kitti/kitti-faster-rcnn-resnest-50.yaml MODEL.WEIGHTS output/model_final.pth)
+
+# (cd detectron2-ResNeSt/ && python3 tools/train_net.py --eval-only \
+#   --config-file configs/kitti/kitti-faster-rcnn-resnest-50.yaml MODEL.WEIGHTS output/model_final.pth)
 
 # Store trained model, github doesn't like big binary file
-mkdir detectron_model -p
+mkdir detectron_model -p || rm detectron_model/*
 (cd detectron_model && split -b 50M ../detectron2-ResNeSt/output/model_final.pth detectron_model_)
