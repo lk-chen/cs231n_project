58b78c43620d5f8c2f6254e2f5922c5476f57699
Linkun Chen <lkchen_github@outlook.com>
2020-05-20 06:26:54 +0000

    Clear tfevents before training

--- try_zhanghang_detectron2.sh
+++ try_zhanghang_detectron2.sh
@@ -13,6 +13,7 @@ head -n 10 lsi-faster-rcnn/data/kitti/lists/val_lsi.txt > detectron2-ResNeSt/dat
 # Use trained model in case you just want to --eval-only
 cat detectron_model/detectron_model_* > detectron2-ResNeSt/output/model_final.pth
 
+rm detectron2-ResNeSt/output/*tfevents*
 # (cd detectron2-ResNeSt/ && python3 tools/train_net.py \
 #   --config-file configs/kitti/kitti-faster-rcnn-resnest-50.yaml MODEL.WEIGHTS output/model_final.pth)
 
--- try_zhanghang_detectron2_template.sh
+++ try_zhanghang_detectron2_template.sh
@@ -8,11 +8,12 @@ cp lsi-faster-rcnn/data/kitti/lists/val_lsi.txt detectron2-ResNeSt/datasets/VOC2
 
 # Only use a few test data
 # head -n 1000 lsi-faster-rcnn/data/kitti/lists/train_lsi.txt > detectron2-ResNeSt/datasets/VOC2012/ImageSets/Main/trainval.txt
-head -n 10 lsi-faster-rcnn/data/kitti/lists/val_lsi.txt > detectron2-ResNeSt/datasets/VOC2012/ImageSets/Main/test.txt
+head -n 1000 lsi-faster-rcnn/data/kitti/lists/val_lsi.txt > detectron2-ResNeSt/datasets/VOC2012/ImageSets/Main/test.txt
 
 # Use trained model in case you just want to --eval-only
 cat detectron_model/detectron_model_* > detectron2-ResNeSt/output/model_final.pth
 
+rm detectron2-ResNeSt/output/*tfevents*
 (cd detectron2-ResNeSt/ && python3 tools/train_net.py \
   --config-file configs/kitti/config_path_placeholder.yaml MODEL.WEIGHTS output/model_final.pth)
 
