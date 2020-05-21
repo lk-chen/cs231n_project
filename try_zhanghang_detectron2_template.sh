848355f091290c76a51d2672febfc80e9894dcad
Linkun Chen <lkchen_github@outlook.com>
2020-05-21 16:14:58 +0000

    Check in model/result of resnest-50/101
    This time trained without image resize (check update in config/kittit)
    
    iter = 10k

--- try_zhanghang_detectron2_template.sh
+++ try_zhanghang_detectron2_template.sh
@@ -11,7 +11,7 @@ cp lsi-faster-rcnn/data/kitti/lists/val_lsi.txt detectron2-ResNeSt/datasets/VOC2
 head -n 2064 lsi-faster-rcnn/data/kitti/lists/val_lsi.txt > detectron2-ResNeSt/datasets/VOC2012/ImageSets/Main/test.txt
 
 # Use trained model in case you just want to --eval-only
-cat detectron_model/detectron_model_* > detectron2-ResNeSt/output/model_final.pth
+cat detectron_model/config_path_placeholder/detectron_model_* > detectron2-ResNeSt/output/model_final.pth
 
 rm detectron2-ResNeSt/output/*tfevents*
 (cd detectron2-ResNeSt/ && python3 tools/train_net.py \
@@ -21,5 +21,5 @@ rm detectron2-ResNeSt/output/*tfevents*
 #   --config-file configs/kitti/config_path_placeholder.yaml MODEL.WEIGHTS output/model_final.pth)
 
 # Store trained model, github doesn't like big binary file
-mkdir detectron_model -p || rm detectron_model/*
-(cd detectron_model && split -b 50M ../detectron2-ResNeSt/output/model_final.pth detectron_model_)
+rm detectron_model/config_path_placeholder/* || mkdir detectron_model/config_path_placeholder -p
+(cd detectron_model/config_path_placeholder && split -b 50M ../../detectron2-ResNeSt/output/model_final.pth detectron_model_)
